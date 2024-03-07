af3d2a8f0ca844b4de70f0e2f96c6a0284e9900c
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-03-07 14:59:04 +0100

    chore: ci configuration

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 var command = `
-    echo "FIGL' a BUCCHIN
+    git tag -a -f v\${nextRelease.version} "v\${nextRelease.version}" -F CHANGELOG.md
+    git push origin "v\${nextRelease.version}"
 `;
 
 var config = require('semantic-release-preconfigured-conventional-commits');
@@ -8,4 +9,5 @@ config.plugins.push(["@semantic-release/exec", { publishCmd: command }],
     "@semantic-release/github",
     "@semantic-release/git",
 )
+config.branches.push(["main"]);
 module.exports = config;
